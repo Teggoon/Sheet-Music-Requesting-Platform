6308e70e71cdb54dc0edcc27a858f5b613b904e4
Haocheng Li <daniel.hc.li@gmail.com>
2019-12-19 15:03:50 -0700

    server receives login page HTML form now

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var fs = require('fs');
+var bodyParser = require('body-parser');
 
 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
 
 
 app.get('/',function(req,res)
 {
-  fs.readFile('home.html', function(err, data) {
+  fs.readFile('login_page.htm', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
@@ -32,7 +34,12 @@ app.get('/client',function(req,res)
   });
 });
 
-
+app.post('/submit_form',function(req,res)
+{
+  console.log("Client sent info:");
+  console.log(req.body.username);
+  console.log(req.body.password);
+});
 var server=app.listen(3000,function() {});
 
 console.log("Server listening on port 3000");
